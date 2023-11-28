25df173f75cb745d388b456ec02cd16fadb3d3b0
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-28 21:09:08 +0530

    Added create event button in events page

--- app/(admin)/admin/events/page.tsx
+++ app/(admin)/admin/events/page.tsx
@@ -1,11 +1,20 @@
 import { Suspense } from 'react';
 import EventsTable from './_components/events-table';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 function Events() {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
-        <EventsTable />
+        <div>
+          <div className='w-full flex justify-end'>
+            <Link href={'/admin/events/create'}>
+              <Button>Create</Button>
+            </Link>
+          </div>
+          <EventsTable />
+        </div>
       </Suspense>
     </div>
   );
