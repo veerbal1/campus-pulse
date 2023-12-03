d1c67617b7b4b927b164f078b94dd08722c8242b
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-03 12:52:21 +0530

    Added QR string validation function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isValidQRCode(qrCode: string) {
+  // Regular expression for the QR code format "event://UUID"
+  const qrCodeRegex =
+    /^event:\/\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+
+  // Test the QR code against the regular expression
+  return qrCodeRegex.test(qrCode);
 }
