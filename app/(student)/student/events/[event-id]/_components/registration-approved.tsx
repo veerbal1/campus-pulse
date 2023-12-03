cc4c80c8927c69e2b7f23ce6d31c4cc3ac97cb07
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-03 09:25:23 +0530

    Showing ticket scanned status

--- app/(student)/student/events/[event-id]/_components/registration-approved.tsx
+++ app/(student)/student/events/[event-id]/_components/registration-approved.tsx
@@ -1,18 +1,33 @@
 import { getUser } from '@/lib/db';
 import QRCode from './qr-code';
+import { cn } from '@/lib/utils';
 
 const RegistrationApproved = async ({ data, eventInfo }: any) => {
   const { rows } = await getUser(data.student_id);
   const studentInfo = rows ? rows[0] : {};
+  console.log('Event Info', data);
+  return <Ticket eventInfo={eventInfo} data={data} studentInfo={studentInfo} />;
+};
 
-  const {} = data;
+const Ticket = ({ eventInfo, data, studentInfo }: any) => {
   return (
     <div className="flex max-w-xs flex-col items-center justify-center gap-2 border p-4 shadow rounded-md">
       <h3 className="scroll-m-20 text-center text-2xl font-semibold tracking-tight">
         {eventInfo.name}
       </h3>
       <div className="main-body flex flex-col">
-        <QRCode qrCode={data.qr_code} />
+        <div
+          className={cn(
+            data.qr_scanned && 'flex justify-center items-center relative'
+          )}
+        >
+          <p className="absolute z-10 text-center text-xl text-green-400">
+            Ticket Scanned <br /> Successfully
+          </p>
+          <div className={cn(data.qr_scanned && 'blur-md grayscale')}>
+            <QRCode qrCode={data.qr_code} />
+          </div>
+        </div>
         <div className="student-detail flex flex-col gap-2 items-center justify-center">
           <div className="flex flex-col items-center justify-center">
             <div className="font-semibold">{studentInfo.name}</div>
@@ -31,8 +46,9 @@ const RegistrationApproved = async ({ data, eventInfo }: any) => {
             </li>
             <li className="text-muted-foreground text-justify text-xs">
               Your presence adds to the joy and spirit of the event. Remember to
-              be kind, respectful, and open to the experiences that await. Let&apos;s
-              make this a beautiful day to remember for everyone involved!
+              be kind, respectful, and open to the experiences that await.
+              Let&apos;s make this a beautiful day to remember for everyone
+              involved!
             </li>
           </ul>
         </div>
